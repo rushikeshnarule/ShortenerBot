4b241a7e7178e0b0a246c0588a85f810edf882c0
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-06-28 12:54:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name='ShortenerBot',
+            session_name='DTG_Shortner_bot',
             api_id=Config.APP_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
