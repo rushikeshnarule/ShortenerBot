578121587148f0781f58829573817442bafe04ac
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-08-31 17:38:01 +0530

    Delete HelperFunc directory

--- HelperFunc/listasstring.py
+++
@@ -1,9 +0,0 @@
-# HuzunluArtemis - 2021 (Licensed under GPL-v3)
-
-def getListAsString(liste, splitter = ","):
-    toret = ""
-    for i, item in enumerate(liste):
-        toret += f"<code>{item}</code>"
-        if i != len(liste)-1: toret += f"{splitter} "
-    return toret
-
