d25851cceea6af9edda3116c3b9882791cad4bb5
GreyMatter's <greymattersyt@gmail.com>
2022-11-27 14:41:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
-
+PORT = environ.get("PORT", "8080")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
