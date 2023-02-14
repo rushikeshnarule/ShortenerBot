83de1fa3b7cb39bc9ec5b15d59e88eac7707e61d
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2023-02-14 16:24:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,9 +68,4 @@ SHORTENER_API = environ.get("SHORTENER_API", None)
 LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL", False)
 
 ## EXTRA FEATURES
-
-     # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
-SELF_DELETE = environ.get('SELF_DELETE', True)
-if SELF_DELETE == "True":
-    SELF_DELETE = True
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
